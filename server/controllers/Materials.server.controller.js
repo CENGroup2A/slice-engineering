c9d27d2a55075e71b05d46874426ce857d44a2c7
desktop <Guerrero Home@DESKTOP-2CK2VDI>
2019-11-05 16:54:08 -0500

    trying to get a response from api for materials

--- server/controllers/Materials.server.controller.js
+++ server/controllers/Materials.server.controller.js
@@ -1,6 +1,26 @@
 var request = require('request');
-request('https://i.materialise.com/web-api/materials?user=lysaght.m@ufl.edu', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) 
-  }
-})
+
+
+ request({
+    url: 'https://i.materialise.com/web-api/materials?user=lysaght.m@ufl.edu', 
+    
+    }, function(error, response, body) {
+    
+      if (error){
+        console.log(error);
+      }
+      else{
+      
+      var data = body;
+      req.results=data.results[0].geometry;
+      console.log(data);
+      }
+      next();
+  });
+
+
+
+exports.MAT = function(req, res) {
+    res.send(req.results)
+};
+module.exports = router;
