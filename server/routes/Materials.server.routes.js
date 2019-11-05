677378bd42618852e0a2876e3c5fbc532edae465
desktop <Guerrero Home@DESKTOP-2CK2VDI>
2019-11-05 16:55:28 -0500

    not working

---
+++ server/routes/Materials.server.routes.js
@@ -0,0 +1,19 @@
+
+/* Dependencies */
+var getMatrerials = require('../controllers/Materials.server.routes.js'), 
+    express = require('express'), //refers to Express the middleware helper for Node.js
+    router = express.Router(); //refers to the Router() function in Express the middleware helper for Node.js
+
+/* 
+  These method calls are responsible for routing requests to the correct request handler.
+  Take note that it is possible for different controller functions to handle requests to the same route.
+ 
+  Note: the listings variable above and the file it is connected to help you trace
+ */
+
+router.route('/mat')
+  .get(getMatrerials.MAT)
+  .post(getMatrerials.MAT)
+  
+
+module.exports = router;
