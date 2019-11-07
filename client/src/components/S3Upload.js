6778c068167acecbda75c925572629f752e3d512
jlomonacouf <jlomonaco@ufl.edu>
2019-11-07 15:40:29 -0500

    Enabled S3 Uploading

--- client/src/components/S3Upload.js
+++ client/src/components/S3Upload.js
@@ -1,57 +1,38 @@
 import React from 'react';
-import DragAndDrop from './DragAndDrop.js'
+//import DragAndDrop from './DragAndDrop.js'
 import axios from 'axios';
 
 class S3Upload extends React.Component {
 
-    // Perform the upload
-    handleUpload = (ev) => {
-        let file = this.uploadInput.files[0];
-        // Split the filename to get the name and type
-        let fileParts = this.uploadInput.files[0].name.split('.');
-        let fileName = fileParts[0];
-        let fileType = fileParts[1];
-        console.log("Preparing the upload");
-        
-        axios.post("http://localhost:5000/api/upload",{
-            fileName : fileName,
-            fileType : fileType
-        })
-        .then(response => {
-            var returnData = response.data.data.returnData;
-            var signedRequest = returnData.signedRequest;
-            var url = returnData.url;
-            this.setState({url: url})
-            console.log("Recieved a signed request " + signedRequest);
-            
-            // Put the fileType in the headers for the upload
-            var options = {
-                headers: {
-                'Content-Type': fileType
-                }
-            };
-            axios.put(signedRequest,file,options)
-            .then(result => {
-                console.log("Response from s3")
-                this.setState({success: true});
-            })
-            .catch(error => {
-                alert("ERROR " + JSON.stringify(error));
-            })
-        })
-        .catch(error => {
-        alert(JSON.stringify(error));
-        })
+    handleChange = (ev) => {
+        this.setState({success: false, url : ""});
     }
 
-    handleDrop = (files) => {
-        for (var i = 0; i < files.length; i++) {
-          if (!files[i].name) return
-          //this.state.files.push(files[i].name)
-          this.addFile(files[i].name)
-        }
-        //this.setState({files: fileList})
-      }
+    upload = (ev) => {
+        var file = this.uploadInput.files[0];
+        axios.post("http://localhost:5000/api/upload", {
+            filename: file.name,
+            filetype: file.type
+        })
+        .then(function (result) {
+            //console.log(result.data.data)
+            var signedUrl = result.data.data;
+            console.log(signedUrl.data)
+            var options = {
+                headers: {
+                'Content-Type': file.type
+                }
+            };
+        
+            return axios.put(signedUrl.data, file, options);
+        })
+        .then(function (result) {
+            console.log(result);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    }
 
     addFile(name) {
         this.props.addFile(name)
@@ -59,19 +40,13 @@ class S3Upload extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Upload a file</h1>
-
-                <input onChange={this.handleChange} ref={(ref) => { this.uploadInput = ref; }} type="file"/>
-                <br/>
-                <button onClick={this.handleUpload}>UPLOAD</button>
-                <h1>Drag and Drop</h1>
-                <DragAndDrop handleDrop={this.handleDrop}>
-                    <div style={{height: 300, width: 250}}>
-                        <p>Hey</p>
-                        <p>Sup</p>
-                    </div>
-                </DragAndDrop>
+            <div className="App">
+                <center>
+                    <h1>UPLOAD A FILE</h1>
+                    <input onChange={this.handleChange} ref={(ref) => { this.uploadInput = ref; }} type="file"/>
+                    <br/>
+                    <button onClick={this.upload}>UPLOAD</button>
+                </center>
             </div>
         )
     }
--- server/controllers/S3Upload.server.controller.js
+++ server/controllers/S3Upload.server.controller.js
@@ -13,7 +13,7 @@ const s3 = new AWS.S3({
 });
 
 // Now lets export this function so we can call it from somewhere else
-exports.sign_s3 = (req,res) => {
+/*exports.sign_s3 = (req,res) => {
     const fileName = req.body.fileName;
     const fileType = req.body.fileType;
     
@@ -21,7 +21,7 @@ exports.sign_s3 = (req,res) => {
     const s3Params = {
         Bucket: BUCKET_NAME,
         Key: fileName,
-        Expires: 500,
+        Expires: 60,
         ContentType: fileType,
         ACL: 'public-read'
     };
@@ -29,8 +29,8 @@ exports.sign_s3 = (req,res) => {
     // Make a request to the S3 API to get a signed URL which we can use to upload our file
     s3.getSignedUrl('putObject', s3Params, (err, data) => {
         if(err){
-        console.log(err);
-        res.json({success: false, error: err})
+            console.log(err);
+            res.json({success: false, error: err})
         }
         // Data payload of what we are sending back, the url of the signedRequest and a URL where we can access the content after its saved. 
         const returnData = {
@@ -41,7 +41,32 @@ exports.sign_s3 = (req,res) => {
         // Send it all back
         res.json({success:true, data:{returnData}});
     });
-}
+}*/
+exports.sign_s3 = (req, res) => {
+    const fileName = req.body.filename;
+    const fileType = req.body.filetype;
+    var params = {
+        Bucket: BUCKET_NAME,
+        Key: fileName,
+        Expires: 60,
+        ContentType: fileType
+    };
+
+    s3.getSignedUrl("putObject", params, function(err, data) {
+        if (err) {
+            console.log(err);
+            return err;
+        } else {
+            const returnData = {
+                signedRequest: data,
+                url: 'https://${BUCKET_NAME}.s3.amazonaws.com/${fileName}'
+            };
+            res.json({success:true, data:{data}});
+            //res.json(data);
+            return data;
+        }
+    });
+};
 
 exports.hello = function(req, res) {
     res.send('world')
