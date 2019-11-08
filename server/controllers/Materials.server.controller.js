f140f35f5120a30f0cb53246d6a60935b8e6c708
desktop <Guerrero Home@DESKTOP-2CK2VDI>
2019-11-07 22:03:43 -0500

    working on api

--- server/controllers/Materials.server.controller.js
+++ server/controllers/Materials.server.controller.js
@@ -1,26 +1,33 @@
-var request = require('request');
+var express = require('express');
+    request = require('request');
+    router = express.Router();
 
+request('https://i.materialise.com/web-api/materials?user=lysaght.m@ufl.edu', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.dirxml(body); // Print the google web page.
+  }else{console.log(error)}
+});
 
- request({
-    url: 'https://i.materialise.com/web-api/materials?user=lysaght.m@ufl.edu', 
+//  request({
+//     url: 'https://i.materialise.com/web-api/materials?user=lysaght.m@ufl.edu', 
     
-    }, function(error, response, body) {
+//     }, function(error, response, body) {
     
-      if (error){
-        console.log(error);
-      }
-      else{
+//       if (error){
+//         console.log(error);
+//       }
+//       else{
       
-      var data = body;
-      req.results=data.results[0].geometry;
-      console.log(data);
-      }
-      next();
-  });
+//       var data = response;
+//       console.log(data);
+//       }
+//       next();
+//   });
 
 
 
-exports.MAT = function(req, res) {
-    res.send(req.results)
-};
-module.exports = router;
+// exports.MAT = function(req, res) {
+//     res.send(req)
+//     console(req)
+// };
+// module.exports = router;
--- server/routes/Materials.server.routes.js
+++ server/routes/Materials.server.routes.js
@@ -1,6 +1,6 @@
 
 /* Dependencies */
-var getMatrerials = require('../controllers/Materials.server.routes.js'), 
+var getMatrerials = require('../controllers/Materials.server.controller.js'), 
     express = require('express'), //refers to Express the middleware helper for Node.js
     router = express.Router(); //refers to the Router() function in Express the middleware helper for Node.js
 
@@ -8,12 +8,13 @@ var getMatrerials = require('../controllers/Materials.server.routes.js'),
   These method calls are responsible for routing requests to the correct request handler.
   Take note that it is possible for different controller functions to handle requests to the same route.
  
-  Note: the listings variable above and the file it is connected to help you trace
+  
  */
 
 router.route('/mat')
-  .get(getMatrerials.MAT)
-  .post(getMatrerials.MAT)
+  //.get(getMatrerials.MAT)
+ // .post(getMatrerials.MAT)
+  
   
 
 module.exports = router;
