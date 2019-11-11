47a654c71c3c9f2314efca85345f2feaf7cf336d
desktop <Guerrero Home@DESKTOP-2CK2VDI>
2019-11-10 21:04:48 -0500

    trying to request price but not sure how

--- server/controllers/Price.server.controller.js
+++ server/controllers/Price.server.controller.js
@@ -2,36 +2,52 @@ var express = require('express');
     request = require('request');
     router = express.Router();
     config = require('../config/config');
+   
+        module.exports = function(req, res, next) {
+            if(req.body) {
+               
+                
+              
+              var options = { 
+                Accept:        ["application/json"],
+                API code:      [config.imaterialize.API],
+                toolId: config.imaterialize.toolId,
+                modelID:"0c3ff00c-3020-4677-9bcf-d451c637252c",
+                materialID:"035f4772-da8a-400b-8be4-2dd344b28ddb",
+                finishID:"bba2bebb-8895-4049-aeb0-ab651cee2597",
+                quantity:"1",
+                scale:"1.0"
+                countryCode: "US",
+                stateCode: "FL",
+                city: "Gainesville",
+                zipCode:  "32608"
+                currency:"USD"
+                 }
+                                 
 
-var options = { 
-    toolId: config.imaterialize.toolId,
-    API: config.imaterialize.API,  
-    }
-
- 
- 
-  request('https://imatsandbox.materialise.net/web-api/pricing',(error, response, body) =>{
-    {
-        "models":[
-           {
-              "modelID":"0c3ff00c-3020-4677-9bcf-d451c637252c",
-              "materialID":"035f4772-da8a-400b-8be4-2dd344b28ddb",
-              "finishID":"bba2bebb-8895-4049-aeb0-ab651cee2597",
-              "quantity":"1",
-              "scale":"1.0"
-           }
-        ],
-        "shipmentInfo": 
-         {
-             "countryCode": "US",
-             "stateCode": "FL",
-             "city": "Gainesville",
-             "zipCode":  "32608"
-         },
-         "currency":"USD"
-     }
-                     
-})
-exports.material = (req, res)=>{
-  res.listOfMats
-}
+                
+          
+              //Setup your request using URL and options - see ? for format
+              POST({
+                url: 'https://imatsandbox.materialise.net/web-api/pricing', 
+                qs: options
+                }, function(error, response, body) {
+                
+                  if (error){
+                    console.log(error);
+                  }
+                  else{
+                  //JSON.parse to get contents. Remember to look at the response's JSON format in open cage data
+                  var data = JSON.parse(body);
+                  console.log(data);
+                  /*Save the coordinates in req.results -> 
+                  
+              
+                   //req.results =
+                  }
+                  next();
+              });
+            } else {
+              next();
+            }
+          };  
