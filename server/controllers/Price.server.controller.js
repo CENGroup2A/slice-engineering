2fb3d38a05bb02359a24e32853df37401c1bdf3e
Jose Guerrero <darkswordss@Joses-MacBook-Pro.local>
2019-11-16 10:56:08 -0500

    save

--- server/controllers/Price.server.controller.js
+++ server/controllers/Price.server.controller.js
@@ -4,12 +4,45 @@ axios = require('axios');
  config = require('../config/config');
 
                   
-//Setup your request using URL and options - see ? for format
+
+
+//Upload model via URL
+axios.post('https://imatsandbox.materialise.net/web-api/tool/2efbcc6f-fe98-406f-8cd1-92b133aae7c3/model', 
+  {
+    // Request should consist of two parts:
+    // • File (Content-Disposition: form-data; name=“file”; filename=“12.csv” Content-Type: application/octet-stream)
+    // • File units (Content-Disposition: form-data; name=“fileUnits”) 
+
+
+    // File: [
+    //   {
+    //     name:"file",
+    //     filename: file.name,
+    //     Content-Type: "application/octet-stream"
+    //   } 
+    // ],
+    fileUrl:"https://static.free3d.com/models/1/dxmuladgj3eo-3DBenchy.stl.zip",
+    fileUnits:"mm",
+    
+      headers: {
+        "accept": "application/json",
+      }
+      
+  })
+    .then((response) => {console.log('response.data', response.data)
+    
+  })
+    .catch((error) => console.error(error));
+    
+    //console.log("md",MODELID)
+
+
+    //get price
 axios.post('https://imatsandbox.materialise.net/web-api/pricing/model', 
 {
   models: [
     {
-      "modelID":"22750b22-fca3-43e0-8803-4acb829d1a30",
+      "modelID": "",
       "materialID":"035f4772-da8a-400b-8be4-2dd344b28ddb",
       "finishID":"bba2bebb-8895-4049-aeb0-ab651cee2597",
       "quantity":"1",
@@ -35,8 +68,6 @@ axios.post('https://imatsandbox.materialise.net/web-api/pricing/model',
   .then((response) => console.log('response.data.models', response.data.models))
   .catch((error) => console.error(error))
 
-
-//ALL I DID WAS SWT UP THE ROUTER /api/price 
 exports.Price = (req, res)=>
 {
   //req.body is the information after we hit "Submit" on the form
@@ -50,24 +81,7 @@ exports.Price = (req, res)=>
   console.log('finish:', finish)
   console.log('file:', file)
 
-  axios.post('https://imatsandbox.materialise.net/web-api/tool/2efbcc6f-fe98-406f-8cd1-92b133aae7c3/model', 
-  {
-    // Request should consist of two parts:
-    // • File (Content-Disposition: form-data; name=“file”; filename=“12.csv” Content-Type: application/octet-stream)
-    // • File units (Content-Disposition: form-data; name=“fileUnits”) 
-
-
-    File: [
-      {
-        name:"file",
-        filename: file.name,
-        Content-Type: "application/octet-stream"
-      } 
-    ],
-    fileUnits:"mm",
-  })
-    .then((response) => console.log('response.data', response.data))
-    .catch((error) => console.error(error))
+  
   
   res.send('data')
 }
