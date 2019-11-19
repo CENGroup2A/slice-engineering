51c69e991b662aa2b9e7e21182057dc1dfbba859
Carlos Carrasquillo <c.carrasquillo@ufl.edu>
2019-11-19 14:23:34 -0500

    added S3Upload.js

--- client/src/components/Accepted.js
+++ client/src/components/Accepted.js
@@ -2,18 +2,25 @@ import React, {Component} from 'react';
 import {Redirect} from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
+const S3Upload = require('./S3Upload.js');
+
 class Accepted extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            file: this.props.location.state.file, //<-------------------------file!!! :)
-            link: null
+            file: this.props.location.state.file, //<-----file
+            link: null,
         };
+        this.submit(this.state.file)
     }
 
     routeChange = () => {
         this.setState({link: '/status'});
+    }
+
+    submit = (files) => {
+      return S3Upload.upload(files);
     }
 
     render() {
@@ -21,7 +28,7 @@ class Accepted extends Component {
         if (link) {
             return <Redirect to={link} />;
         }
-        
+
         return (
             <div>
                 <p>TO DO FOR CARLOS: SEND FILE TO USE IN UPLOAD COMPONENT</p>
---
+++ client/src/components/S3Upload.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+module.exports.upload = (file) => {
+  axios.post("http://localhost:5000/api/upload", {
+      filename: file.name,
+      filetype: file.type
+  })
+  .then(function (result) {
+      var signedUrl = result.data.data;
+      var options = {
+          headers: {
+          'Content-Type': file.type
+          }
+      };
+
+      return axios.put(signedUrl.data, file, options)
+  })
+  .then(function (result) {
+      if(result.statusText === "OK") {
+          console.log('File uploaded')
+          //this.props.changeFile(file)
+          //this.props.changeUploadStatus(true)
+      }
+  })
+  .catch(function (err) {
+      console.log(err);
+      //this.changeUploadStatus()
+      //this.props.changeUploadStatus(false)
+  });
+}
