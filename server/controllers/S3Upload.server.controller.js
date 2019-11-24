330a19b125ab0bc5cca2f9dae943e86010cc2c01
Raymond Bernardo <Seth10001@users.noreply.github.com>
2019-11-24 15:46:40 -0500

    Update variables to check for system variables as required by heroku

--- server/controllers/S3Upload.server.controller.js
+++ server/controllers/S3Upload.server.controller.js
@@ -1,11 +1,10 @@
 //const Example = require('../models/examples.server.model.js')
 const AWS = require('aws-sdk');
 const fs = require('fs');
-const config = require('../config/config')
 
-const ID = config.key.keyID;
-const SECRET = config.key.secretKey;
-const BUCKET_NAME = config.bucket.bucketName;
+const ID = process.env.S3_KEY_ID || require('../config/config').key.keyID;
+const SECRET = process.env.S3_SECRETE_KEY || require('../config/config').key.secretKey;
+const BUCKET_NAME = process.env.S3_BUCKET_NAME || require('../config/config').bucket.bucketName;
 
 const s3 = new AWS.S3({
     accessKeyId: ID,
