2533c1d5d0ca78a3bd4aca91eec33f9d322d5e68
kevinngn99 <kevinnguyen1@ufl.edu>
2019-11-27 20:33:39 -0500

    New Home Page

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -3,8 +3,26 @@ import React from 'react';
 
 function Home() {
     return (
-        <div>
-            <p>Welcome to Slice Engineering</p>
+        <div style={{ height: "75vh", width: "100%", display: "flex", justifyContent: 'center', alignItems: 'center', backgroundColor : "#FFFFFF"}}>
+            <ul>
+                <li>
+                    <p align="center" style={{fontSize: "45px"}}>
+                        Thank you for visiting the Slice Engineering 3D Printing Service Portal.
+                    </p>
+                </li>
+                <li>
+                    <p align="center" style={{fontSize: "25px"}}>
+                        <br/>
+                        We have a variety of printing methods, materials, and finishes available to choose from.
+                        <br/>
+                        Receive a real time quote by simply uploading your CAD file, selecting the desired 3D printing method,
+                        <br/>
+                        material, surface finish, and inputting a shipping address.
+                        <br/>
+                        All quotes include shipping.
+                    </p>
+                </li>
+            </ul>
         </div>
     );
 }
