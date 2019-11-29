5f08eca43d2f15b7164287735f7e6ed0a3bd953d
kevinngn99 <kevinnguyen1@ufl.edu>
2019-11-28 22:15:26 -0500

    Material Component Redesign

--- client/src/components/Quote/Material.js
+++ client/src/components/Quote/Material.js
@@ -44,7 +44,7 @@ class Material extends React.Component {
             materialsList: [''],
             finishList: ["Natural white","Polished natural white","Dyed yellow","Satin Yellow","Polished and Dyed Yellow","Dyed orange","Satin Orange","Polished and Dyed Orange","Dyed red","Satin Red","Polished and Dyed Red","Satin Green","Polished and Dyed Green","Dyed blue","Satin Blue","Polished and Dyed Blue","Dyed purple","Satin Purple","Polished and Dyed Purple","Dyed black","Satin black","Polished and Dyed Black","Dyed bordeaux","Satin Bordeaux","Polished and Dyed Bordeaux","Dyed petrol blue","Satin Petrol Blue","Polished and Dyed Petrol blue","Dyed brown","Satin Brown","Polished and Dyed Brown","Velvet yellow","Velvet ochre","Velvet pink","Velvet bordeaux","Velvet green","Velvet petrol blue","Velvet blue","Velvet black","Spray painted white","Spray painted black","Waterproof white","Dyed green","Dyed grey"],
             mats: [],
-            price: '',
+            price: '0.00',
             scale: '1',
             //Needed to send to Price.server.controller
             countryCode: 'US',
@@ -168,6 +168,10 @@ class Material extends React.Component {
     }
 
     handleSubmit(event) {
+        var text = document.getElementById('ui-price');
+        text.textContent = "Calculating";
+        document.getElementById('wave').style.display = '';
+
         event.preventDefault();
 
         const reactData = {material: materialzID, finish: finishID, countryCode: this.state.countryCode, stateCode: this.state.stateCode, city: city, zipcode: zipcode, currency: currency, scale: this.state.scale}
@@ -185,8 +189,14 @@ class Material extends React.Component {
                 console.log('price.data', price.data)
                 this.setState({price: price.data.totalPrice})
                 this.setState({modelID: price.data.modelID})
+                text.textContent = "Total Price: $" + this.state.price;
+                document.getElementById('wave').style.display = 'none';
             })
-            .catch(err => console.log('error', err.data))
+            .catch((err) => {
+                text.textContent = "An error has occured. Please refresh the page!";
+                document.getElementById('wave').style.display = 'none';
+                console.log('error', err.data)
+            })
     }
 
     handleChangeScale(event){
@@ -196,68 +206,85 @@ class Material extends React.Component {
 	render() {
 		return (
             //If the materials aren't in the array, don't render
-			<div>
-                <p>Scale: </p>
+            <div style={{ height: "100vh", width: "50%", float: "right", display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor : "#FFFFFF"}}>
+                <div style={{ display: 'flex', justifyContent: 'left', alignItems: 'left', height: "85%", width: "85%", backgroundColor: "#FFFFFF" }}>
                 <div>
-                    <Slider min={20} defaultValue={100} marks={{ 20: 20, 50: 50, 70: 70, 100: 100 }} step={null} onAfterChange={this.handleChangeScale}/>
-                    <p></p>
+                    <div id="ui-text">Scale</div>
+                    <div>
+                        <Slider min={20} defaultValue={100} marks={{ 20: 20, 50: 50, 70: 70, 100: 100 }} step={null} onAfterChange={this.handleChangeScale}/>
+                    </div>
+                    <br/>
+                    <div>
+                        <div id="ui-text">Material</div>
+                        <DropdownButton id="but-material" title="Select" onSelect={this.handleChangeMaterial}>
+                            <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
+                                {this.state.materialsList.map((x, y) => <Dropdown.Item eventKey={[y, x]}>{x}</Dropdown.Item>)}
+                            </div>
+                        </DropdownButton>
+
+                        <div id="ui-text">Finish</div>
+                        <DropdownButton disabled={this.state.finishState} id="but-finish" title={this.state.finishList[0]} onSelect={this.handleChangeFinish}>
+                            <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
+                                {this.state.finishList.map((x, y) => <Dropdown.Item eventKey={[y, x]}>{x}</Dropdown.Item>)}
+                            </div>
+                        </DropdownButton>
+
+                        <div id="ui-text">Country</div>
+                        <DropdownButton id="but-country" title="Select" onSelect={this.handleChangeCountry}>
+                            <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
+                                {countryCodes.map((x, y) => <Dropdown.Item eventKey={[y, x]}>{x}</Dropdown.Item>)}
+                            </div>
+                        </DropdownButton>
+
+                        <div id="ui-text">State</div>
+                        <DropdownButton id="but-state" title="Select" onSelect={this.handleChangeState}>
+                            <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
+                                {stateCodes.map((x, y) => <Dropdown.Item eventKey={[y, x]}>{x}</Dropdown.Item>)}
+                            </div>
+                        </DropdownButton>
+
+                        <div id="ui-text">City</div>
+                        <input autoComplete="off" id="but-city" type="text" name="city" placeholder="CITY" onChange={this.handleChangeCity}/>
+
+                        <div id="ui-text">Zip Code</div>
+                        <input autoComplete="off" id="but-zip" type="text" name="Zipcode" placeholder="ZIP CODE" onChange={this.handleChangeZipcode}/>
+
+                        <div id="ui-text">Currency</div>
+                        <DropdownButton id="but-currency" title="Select" onSelect={this.handleChangeCurrency}>
+                            <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
+                                <Dropdown.Item eventKey={"USD"}>USD</Dropdown.Item>
+                            </div>
+                        </DropdownButton>
+                        
+                        <div>
+                            <ul>
+                                <li>
+                                    <Button id="ui-submit" type="submit" onClick={this.handleSubmit}> Request Quote </Button>
+                                </li>
+
+                                <li style={{float: "right"}}>
+
+                                    <ul style={{display: 'flex', alignItems: 'center'}}>
+                                        <li>
+                                            <div id="ui-price">
+                                                Total Price: ${this.state.price} 
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div style={{display: "none"}} id="wave">
+                                                <span class="dot"></span>
+                                                <span class="dot"></span>
+                                                <span class="dot"></span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
                 </div>
-                
-                <div>
-                    <div id="ui-text">Material</div>
-                    <DropdownButton id="but-material" title="Select" onSelect={this.handleChangeMaterial}>
-                        <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
-                            {this.state.materialsList.map((x, y) => <Dropdown.Item eventKey={[y, x]}>{x}</Dropdown.Item>)}
-                        </div>
-                    </DropdownButton>
-
-                    <div id="ui-text">Finish</div>
-                    <DropdownButton disabled={this.state.finishState} id="but-finish" title={this.state.finishList[0]} onSelect={this.handleChangeFinish}>
-                        <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
-                            {this.state.finishList.map((x, y) => <Dropdown.Item eventKey={[y, x]}>{x}</Dropdown.Item>)}
-                        </div>
-                    </DropdownButton>
-
-                    <div id="ui-text">Country</div>
-                    <DropdownButton id="but-country" title="Select" onSelect={this.handleChangeCountry}>
-                        <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
-                            {countryCodes.map((x, y) => <Dropdown.Item eventKey={[y, x]}>{x}</Dropdown.Item>)}
-                        </div>
-                    </DropdownButton>
-
-                    <div id="ui-text">State</div>
-                    <DropdownButton id="but-state" title="Select" onSelect={this.handleChangeState}>
-                        <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
-                            {stateCodes.map((x, y) => <Dropdown.Item eventKey={[y, x]}>{x}</Dropdown.Item>)}
-                        </div>
-                    </DropdownButton>
-
-                    <div id="ui-text">City</div>
-                    <input id="but-city" type="text" name="city" placeHolder="CITY" onChange={this.handleChangeCity}/>
-
-                    <div id="ui-text">Zip Code</div>
-                    <input id="but-zip" type="text" name="Zipcode" placeHolder="ZIP CODE" onChange={this.handleChangeZipcode}/>
-
-                    <div id="ui-text">Currency</div>
-                    <DropdownButton id="but-currency" title="Select" onSelect={this.handleChangeCurrency}>
-                        <div id="scroll" style={{width: "500px", overflowY: "scroll", maxHeight: "315px"}}>
-                            <Dropdown.Item eventKey={"USD"}>USD</Dropdown.Item>
-                        </div>
-                    </DropdownButton>
-
-                    <Button id="ui-submit" type="submit" onClick={this.handleSubmit}>
-                        Request Quote
-                    </Button>
-
-                    <p>Total Price: {this.state.price} </p>
-                </div>
-                <div>
-                    <input type="file" name="file" onChange={this.onChangeFileUpload}/>
-
-                    <button><Link to="/cart">Go to Cart</Link></button>
-                    
-                </div>
-			</div>
+            </div>
 		);
 	}
 }
--- client/src/views/MaterialView.js
+++ client/src/views/MaterialView.js
@@ -3,10 +3,7 @@ import Material from "../components/Quote/Material"
 
 const MaterialView = () => {
         return (
-            <div>
-                <h1>The new material page</h1>
-                <Material />
-            </div>
+            <Material />
         );
 }
 
