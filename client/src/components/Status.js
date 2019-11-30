460c25eb2066b46e2567960df6cb3e1232626772
Raymond Bernardo <Seth10001@users.noreply.github.com>
2019-11-29 22:03:12 -0500

    Revert "Cad render"

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -1,12 +1,7 @@
 import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button';
-const axios = require('axios');
 
 class Status extends Component {
-    constructor(props) {
-        super(props);
-        axios.post("http://localhost:5000/api/getS3");
-    }
     render() { //---------------------- look here: https://getbootstrap.com/docs/3.3/components/ ----------------- cool stuff :) -------------
         return ( //-----also if u wanna use the components u need to import them ---- like the button on the top of this file
             <div>
--- server/routes/S3upload.server.routes.js
+++ server/routes/S3upload.server.routes.js
@@ -2,10 +2,7 @@ const server = require('../controllers/S3Upload.server.controller.js'),
     express = require('express'),
     router = express.Router()
 
-router.route('/upload')
+router.route('/')
   .post(server.sign_s3);
-
-router.route('/getS3')
-  .post(server.get_s3);
 
 module.exports = router;
