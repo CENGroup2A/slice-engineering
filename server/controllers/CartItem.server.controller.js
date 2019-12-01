7a8e2b0704a07cc9c5a7f8feb419953b7d63b184
Raymond Bernardo <Seth10001@users.noreply.github.com>
2019-12-01 15:45:15 -0500

    Update to send multipart form

--- server/controllers/CartItem.server.controller.js
+++ server/controllers/CartItem.server.controller.js
@@ -4,40 +4,53 @@ axios = require('axios');
  config = require('../config/config');
 
 var cartItem="";
+var FormData = require('form-data');
 
-// /getCartItem
-async function FetchCartItem(){
-  let data =await axios.post('https://imatsandbox.materialise.net/web-api/cartitems/register', 
-  {
+function FetchCartItem()
+{
+  var example = {
     "cartItems":[
-      {
-        "toolID":"2efbcc6f-fe98-406f-8cd1-92b133aae7c3",
-        "MyCartItemReference":"some reference",
-        "modelID":"8a07f8c7-eda7-4a15-a843-f7661491c2d8",
-        "modelFileName":"",
-        "fileUnits":"mm",
-        "fileScaleFactor":"1",
-        "materialID":"035f4772-da8a-400b-8be4-2dd344b28ddb",
-        "finishID":"bba2bebb-8895-4049-aeb0-ab651cee2597",
-        "quantity":"1",
-        "xDimMm":"12",
-        "yDimMm":"12",
-        "zDimMm":"12",
-        "volumeCm3":"2.0",
-        "surfaceCm2":"100.0",
-        "iMatAPIPrice": "14.19",
-        "mySalesPrice": "16.0",
-      }
+       {
+          "toolID":"2efbcc6f-fe98-406f-8cd1-92b133aae7c3",
+          "MyCartItemReference":"some reference",
+          "modelID":"8a07f8c7-eda7-4a15-a843-f7661491c2d8",
+          "modelFileName":"",
+          "fileUnits":"mm",
+          "fileScaleFactor":"1",
+          "materialID":"035f4772-da8a-400b-8be4-2dd344b28ddb",
+          "finishID":"bba2bebb-8895-4049-aeb0-ab651cee2597",
+          "quantity":"1",
+          "xDimMm":"12",
+          "yDimMm":"12",
+          "zDimMm":"12",
+          "volumeCm3":"2.0",
+          "surfaceCm2":"100.0",
+          "iMatAPIPrice": "25.0",
+          "mySalesPrice": "26.0",
+       }
     ],
-    "currency":"USD",
-    "headers": {
-      "accept": "application/json",
-      "Content-Type" : "multipart/form-data", 
-    }
+    "currency":"USD"
+ }
+ var form = new FormData()
+ form.append("data", JSON.stringify(example), {filename:"blob", contentType: 'application/json'})
+ console.log(form)
+
+  return axios.post('https://imatsandbox.materialise.net/web-api/cartitems/register', 
+  form,
+  {
+    headers: form.getHeaders()
   })
-  console.log("data.data",data.data);
-  cartItem = data.data;
-  return(data.data.modelID)
+  .then((data) =>
+  {
+    console.log("data.data",data.data);
+    cartItem=data.data;
+    return Promise.resolve(data.data.modelID)
+  })
+  .catch((error) =>
+  {
+    console.error(error)
+  })
+  
 }
 
 exports.sendCartItem = (req, res)=>
