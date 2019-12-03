4df0b67ab1114754e5ed84f43340d6a725d63763
jlomonacouf <jlomonaco@ufl.edu>
2019-12-02 19:44:40 -0500

    Added folder for each user

--- server/middleware/isAuth.js
+++ server/middleware/isAuth.js
@@ -1,7 +1,9 @@
 function isAuth(req, res, next)
 {
     if (req.isAuthenticated())
+    {
         next()
+    }
     else
         res.json({"auth": false, "message": "User is not authenticated."});
 
